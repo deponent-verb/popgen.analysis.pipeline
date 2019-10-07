4db70d8ccfa24ecb1d3d4f63cea63c16c1e5a19a
work-Anthony <shingkwong@staff-249-146.wireless.adelaide.edu.au>
2019-10-08 09:59:01 +1030

    update

--- toy_pipeline.R
+++ toy_pipeline.R
@@ -6,3 +6,17 @@ df<-generate_df(sim_list = data,win_split = 10)
 
 sim<-data[[414]]
 sub_win(sim$genomes,2)
+
+#Reading in large dataset ----
+hard<-readRDS("~/work/MPhil/data/hard.rds")
+soft<-readRDS("~/work/MPhil/data/soft.rds")
+neutral<-readRDS("~/work/MPhil/data/neutral.rds")
+
+data<-list(hard,neutral,soft)
+data<-unlist(data,recursive = F)
+saveRDS(data,"~/work/MPhil/data/fiveK.rds")
+
+df<-readRDS("~/work/MPhil/data/fiveK.rds")
+
+df<-generate_df(sim_list = df,win_split = 10)
+
