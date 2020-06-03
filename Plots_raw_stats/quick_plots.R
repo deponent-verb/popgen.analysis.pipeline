4bb4bc93f8d85ff0b2470a561b33fe819c74ef7f
Christian Huber <christian.domitian.huber@gmail.com>
2020-06-03 14:10:24 +0930

    Some plot commands changed

--- Plots_raw_stats/quick_plots.R
+++ Plots_raw_stats/quick_plots.R
@@ -1,6 +1,11 @@
 library(ggplot2)
 library(data.table)
 library(cowplot)
+library(tidymodels)
+library(tidyverse)
+library(skimr)
+library(recipes)
+library(forcats)
 
 setwd("~/repos/popgen.analysis.pipeline/Plots_raw_stats/")
 
@@ -21,7 +26,7 @@ ggplot(vip, aes(factor(Window), Importance, col=model)) + geom_point() + geom_li
 
 # Looking at raw data
 
-raw = fread(input = "../data/0.25ds_set.csv")
+raw = fread(input = "../data/0.25ds_snp_set.csv")
 
 raw[, t1 := bottle_time1]
 raw[, s := bottle_size1]
@@ -103,5 +108,93 @@ p_list = lapply(listOfStats, function (x) {
 })
 
 plot_grid(plotlist = p_list, nrow = 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################
+######################
+######################
+#### PCA
+
+genomes = fread(input = "../data/0.25ds_set.csv")
+
+#Data cleaning. Take out bottleneck info and selection coefficient for now. ----
+genome_SS  <- genomes %>% 
+  #filter (demography == 'cpop') %>%
+  filter (s_coef %in% c(0, 0.005)) %>%
+  dplyr::select(sweep, H_1:h123_11)
+genome_SS
+
+skimr::skim(genome_SS)
+
+ #PCA ----
+
+#We will make a recipe to transform our data into their PCs.
+
+pc_all <- recipe(data = genome_SS, sweep + demography ~.) %>% #set our group to be sweep
+  step_normalize(all_predictors()) %>%  #standardise all predictors to have mean 0, var 1.
+  step_pca(all_predictors()) %>% #compute PCs
+  prep() #give it the data to compute PC's.
+
+tidy_pca <- tidy(pc_all, 2) #take the loadings on each PC. Entered 2 because our recipe had 2 steps.
+
+tidy_pca = data.table(tidy_pca)
+tidy_pca[, Window := as.numeric(gsub(terms, pattern = ".*_(\\d+)$", replacement = "\\1"))]
+tidy_pca[, Stat := gsub(terms, pattern = "(.*)_\\d+$", replacement = "\\1")]
+
+ggplot(tidy_pca[tidy_pca$component %in% c("PC1","PC2"),], aes(Window, value, col=Stat)) + geom_line() + geom_point() +facet_grid(component ~ Stat) + geom_hline(yintercept = 0, lty=2)
+
+
+
+
+#We can now transform our data into PCs, using our recipe. 
+
+PCA_all_demos = list()
+for (demo in unique(genomes$demography)) {
+  genome_SS  <- genomes %>% 
+    filter (demography == demo) %>%
+    filter (s_coef %in% c(0, 0.005)) %>%
+    dplyr::select(demography, sweep, H_1:h123_11)
+  PCA_temp = bake(pc_all, genome_SS)
+  if (demo == "cpop") {
+    PCA_temp$t1 = 0
+    PCA_temp$t2 = 0
+    PCA_temp$s = 1
+    PCA_temp$duration = 0
+  } else {
+    PCA_temp$t1 = as.numeric(gsub(PCA_temp$demography, pattern = "t1:(\\d+)_.*", replacement = "\\1"))
+    PCA_temp$t2 = as.numeric(gsub(PCA_temp$demography, pattern = ".+t2:(\\d+)$", replacement = "\\1"))
+    PCA_temp$s = as.numeric(gsub(PCA_temp$demography, pattern = ".*s:(\\d\\.\\d+)_.*", replacement = "\\1"))
+    PCA_temp$duration = PCA_temp$t1 - PCA_temp$t2
+  }
+  PCA_all_demos[[demo]] = PCA_temp
+}
+
+
+genome_PCA_all = rbindlist(PCA_all_demos)
+
+#We can now plot our data with the first 2 PC's. Group by sweep.
+
+genome_PCA_all %>% 
+  filter (demography != "cpop") %>% 
+  #specify PC's as axis. Group by sweep.
+  ggplot(aes(x = PC1, y = PC2, col = sweep)) + 
+  geom_point(alpha = 0.01) + 
+  geom_density_2d() + 
+  scale_color_brewer(palette = "Set1") +
+  ggtitle("PCA plot using all predictors") +
+  facet_grid(t2 ~ duration + s) +
+  theme(plot.title = element_text(hjust = 0.5)) 
+
 
 
