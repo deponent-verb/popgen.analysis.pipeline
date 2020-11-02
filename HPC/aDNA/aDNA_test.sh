905e6b3b284dd5250e019a881b6e81bba3906551
work-Anthony <shingkwong@192-168-1-133.tpgi.com.au>
2020-11-02 12:45:26 +1030

    Update aDNA_test.sh

--- HPC/aDNA/aDNA_test.sh
+++ HPC/aDNA/aDNA_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -p batch           	                                # partition (this is the queue your job will be added to) 
+#SBATCH -p test          	                                # partition (this is the queue your job will be added to) 
 #SBATCH -N 1               	                                # number of nodes (no MPI, so we only use a single node)
 #SBATCH -n 6              	                                # number of cores
 #SBATCH --time=1:00:00    	                                # walltime allocation, which has the format (D-HH:MM:SS), here set to 1 hour
-#SBATCH --mem=10GB         	                                # memory required per node (here set to 4 GB)
+#SBATCH --mem=2GB         	                                # memory required per node (here set to 4 GB)
 
 # Notification configuration 
 #SBATCH --mail-type=END					    	# Send a notification email when the job is done (=END)
