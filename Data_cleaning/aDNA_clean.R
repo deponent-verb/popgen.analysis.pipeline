23de4d4cd470854d1aaca09d2680e28ee653451f
work-Anthony <shingkwong@cmcs-iMac.local>
2020-11-10 19:34:21 +1030

    changes to plots

--- Data_cleaning/aDNA_clean.R
+++ Data_cleaning/aDNA_clean.R
@@ -4,9 +4,9 @@ library(tidyverse)
 
 #read in data
 #ancient_genomes<-read_csv("~/work/MPhil/ml_review/ancient_data/ancient_df/ancient_cpop1.csv")
-#ancient_genomes<-read_csv("~/work/MPhil/ml_review/ancient_data/ancient_df/aDNA_cpop.csv")
+ancient_genomes<-read_csv("~/work/MPhil/ml_review/ancient_data/ancient_df/aDNA_cpop.csv")
 #ancient_genomes<-read_csv("~/work/MPhil/ml_review/ancient_data/ancient_df/aDNA_small.csv")
-ancient_genomes<-read_csv("~/work/MPhil/ml_review/ancient_data/ancient_df/test_set1.csv")
+#ancient_genomes<-read_csv("~/work/MPhil/ml_review/ancient_data/ancient_df/test_set1.csv")
 
 #filter out unneeded params
 ancient_genomes <- subset(ancient_genomes, 
@@ -18,6 +18,6 @@ ancient_genomes <- subset(ancient_genomes,
 
 #write_csv(ancient_genomes,"~/Documents/GitHub/popgen.analysis.pipeline/data/cleaned_aDNA.csv")
 
-#write_csv(ancient_genomes,"~/Documents/GitHub/popgen.analysis.pipeline/data/cleaned_aDNA.csv")
-write_csv(ancient_genomes,"~/Documents/GitHub/popgen.analysis.pipeline/data/cleaned_testset1.csv")
+write_csv(ancient_genomes,"~/Documents/GitHub/popgen.analysis.pipeline/data/cleaned_aDNA.csv")
+#write_csv(ancient_genomes,"~/Documents/GitHub/popgen.analysis.pipeline/data/cleaned_testset1.csv")
 
